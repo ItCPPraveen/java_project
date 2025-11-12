f48e553172104e0d4188d61e5c47c4c79f3b5f00
ItCPPraveen <cppraveen02@gmail.com>
2025-11-12 22:44:24 +0530

    description added - praveen

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello from IntelliJ Spring Boot!";
+    @GetMapping("/api/description")
+    public String description() {
+        return "I am a frontend developer with a passion for Software Engineering. I have 6 months of internship experience as UI developer. My journey has been guided by an insatiable curiosity and a desire to continuously learn and grow.";
     }
 }
